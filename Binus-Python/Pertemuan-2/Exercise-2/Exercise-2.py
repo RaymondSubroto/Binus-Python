981b5c409491d20af83b09b23d8c2a6b59b8292f
RaymondSubroto <raymondkenzies5@gmail.com>
2026-08-13 17:23:55 +0700

    Add files via upload

---
+++ Binus-Python/Pertemuan-2/Exercise-2/Exercise-2.py
@@ -0,0 +1,4 @@
+print("Menghitung Volume Bola")
+PI = 3.14
+r = int(input("r = "))
+print("Volume =", 4/3 * PI * r ** 3)
---
+++ Binus-Python/Pertemuan-2/Exercise-3/Exercise-3.py
@@ -0,0 +1,5 @@
+print("Menghitung Volume Tabung")
+PI = 3.14
+r = int(input("r = "))
+t = int(input("t = "))
+print("Volume =", PI * r ** 2 * t)
